de3a4c426773308539c8188fc4daf8ad88213574
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-03-03 08:36:15 -0300

    Add Exercicio4.java to calculate sum of numbers
    Implement a program that calculates the sum of numbers from 0 to 99.

---
+++ aula2/Lista2/Exercicio4.java
@@ -0,0 +1,18 @@
+package pkg;
+
+import java.util.Scanner;
+
+public class lista2_exercicio4 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		int soma = 0;
+		
+		for (int i = 0; i < 100; i++) {
+			soma += i;
+			System.out.println("Soma dos numeros: "+soma);
+		}
+	}
+
+}
