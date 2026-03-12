67a21bd8d39bc265836d615c642fbab8d82e00d1
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-03-12 08:19:37 -0300

    Create Pessoa.java

---
+++ aula3/Lista4/Exercicio3/Pessoa.java
@@ -0,0 +1,16 @@
+package pkg;
+
+public class Pessoa {
+
+	public static void main(String[] args) {
+		String nome = "Guilherme";
+		int idade = 19;
+		String genero = "Masculino";
+		
+		System.out.println("Nome: "+nome);
+		System.out.println("Idade: "+idade);
+		System.out.println("Genero: "+genero);
+		
+	}
+
+}
