17cd43674b57f5f8f803862b19e19ab92ae21b51
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-03-12 08:24:48 -0300

    Create Livro.java

---
+++ aula3/Lista4/Exercicio 4/Livro.java
@@ -0,0 +1,16 @@
+package pkg;
+
+public class Livro {
+
+	public static void main(String[] args) {
+		String titulo = "The Wild Robot";
+		String autor = "Peter Brown";
+		String anoPublicacao = "05/04/2016";
+		
+		System.out.println("Titulo: "+titulo);
+		System.out.println("Autor: "+autor);
+		System.out.println("Ano de publicação: "+anoPublicacao);
+
+	}
+
+}
