9f459ab4d6fa758c8a316aee82cb17d2cfb9d29c
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-03-22 18:21:57 -0300

    Create Pessoa.java

---
+++ aula4/Lista 5/Exercicio 5/Pessoa.java
@@ -0,0 +1,22 @@
+package pkg;
+
+public class Pessoa {
+	public String nome;
+	public int idade;
+	
+	public Pessoa(String nome, int idade) {
+		this.nome = nome;
+		this.idade = idade;
+		
+		System.out.println("Construtor com nome e idade: ");
+		System.out.println("Nome: "+nome);
+		System.out.println("Idade: "+idade);
+	}
+	
+	public Pessoa(int idade) {
+		this.idade = idade;
+		
+		System.out.println("Construtor apenas com idade:");
+        System.out.println("Idade: " + idade);
+	}
+}
