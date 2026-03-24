0e648048ff42cdf550aea13672b330a99af02d0a
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-03-24 09:57:18 -0300

    Update Exercicio 7.java

--- aula 5/Lista 6/Exercicio 7.java
+++ aula 5/Lista 6/Exercicio 7.java
@@ -8,3 +8,27 @@
 // FileNotFoundException: Quando o código tenta abrir um arquivo que não está lá.
 // SQLException: Erros ao interagir com bancos de dados.
 
+package pkg;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		try {
+			int numeros[] = {1, 2, 3};
+			System.out.println(numeros[3]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Erro: indice fora do array\n" +e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Exceção: "+e.toString());
+		}
+		
+		try {
+			int a = 10;
+			int b = 0;
+			System.out.println(a/b);
+		} catch (ArithmeticException e) {
+			System.out.println("Erro: Exceção aritmetica\n" +e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Exceção: "+e.toString());
+		}
