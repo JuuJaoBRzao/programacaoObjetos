a9bd4e5b4c206f327ea88f84f286f6956db180e2
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-03-24 10:30:44 -0300

    Create Exercicio 2.java

---
+++ aula 5/Lista 6/Exercicio 2.java
@@ -0,0 +1,32 @@
+package pkg;
+
+import java.util.InputMismatchException;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+		
+		try {
+			System.out.println("Digite o primeiro numero: ");
+			int n1 = teclado.nextInt();
+			System.out.println("Digite o segundo numero: ");
+			int n2 = teclado.nextInt();
+			
+			System.out.println("Soma: "+(n1 + n2));
+			System.out.println("Subtração: "+(n1 - n2));
+			System.out.println("Multiplicação: "+(n1 * n2));
+			System.out.println("Divisão: "+(n1 / n2));
+			
+		} catch (ArithmeticException e) {
+			System.out.println("Erro: Numero dividido por zero");
+		} catch (InputMismatchException e) {
+			System.out.println("Erro: Digite apenas numeros inteiros");
+		}
+		
+		teclado.close();
+	}
+
+}
