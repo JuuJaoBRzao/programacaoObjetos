2ebb558a56b3f458d738d33532589c517aefdf0a
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-03-24 09:56:50 -0300

    Create Exercicio 7.java

---
+++ aula 5/Lista 6/Exercicio 7.java
@@ -0,0 +1,10 @@
+// ArithmeticException:	Erros matemáticos, como divisão por zero.
+// NullPointerException: A famosa "NPE". Ocorre quando você tenta usar um objeto que está nulo.
+// ArrayIndexOutOfBoundsException:	Quando você tenta acessar uma posição do array que não existe.
+// ClassCastException:	Tentativa inválida de converter um objeto de um tipo para outro.
+// NumberFormatException: Tentar converter uma String (tipo "ABC") em número.
+// IllegalArgumentException:	Quando um método recebe um argumento que não faz sentido.
+// IOException: Falha geral de entrada/saída (leitura de arquivos, rede).
+// FileNotFoundException: Quando o código tenta abrir um arquivo que não está lá.
+// SQLException: Erros ao interagir com bancos de dados.
+
