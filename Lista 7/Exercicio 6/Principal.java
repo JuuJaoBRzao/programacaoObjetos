09462055320da444e7d71c19b2a34689c8ea10b6
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-04-04 13:11:16 -0300

    Create Principal.java

---
+++ Lista 7/Exercicio 6/Principal.java
@@ -0,0 +1,20 @@
+package pkg;
+
+public class Principal {
+    public static void main(String[] args) {
+        Carro c = new Carro("Fusca Turbo", 20, 120);
+
+        System.out.println("Carro: " + c.modelo);
+
+        c.acelerar(50); 
+        c.acelerar(100); 
+
+        if (c.atingiuVelocidadeMaxima()) {
+            System.out.println("Atenção: Você está no limite do motor!");
+        }
+
+        c.calcularTempoViagem(240); 
+
+        c.frear(150); /
+    }
+}
