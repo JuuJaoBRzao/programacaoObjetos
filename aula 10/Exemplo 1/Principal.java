d110de59554fbc79255e338e61b669dce04244ea
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-04-28 09:00:27 -0300

    Add Principal class with Animal and Cachorro instances

---
+++ aula 10/Exemplo 1/Principal.java
@@ -0,0 +1,19 @@
+package pkg;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Animal a = new Animal();
+		a.nome ="Cachorro";
+		
+		Cacchorro c = new Cacchorro();
+		c.nome = "dçsfkçlfsk";
+		c.idade = 20;
+		c.som = "pkeleqirwokie";
+		c.raca = "lkdjlajklmn";
+		c.exibir();
+		c.latir();
+		
+	}
+
+}
