bdef0ed825e91f89c543a3f022bfd65d24ea3b77
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-04-28 10:40:44 -0300

    Add Principal class with main method

---
+++ aula 10/Exemplo 3/Principal.java
@@ -0,0 +1,9 @@
+package pkg;
+
+public class Principal {
+	public static void main(String[] args) {
+		Quadrado q = new Quadrado(100, 150, "Quadrado do Ricardo", "Ricardo Frohlich");
+		q.exibeDados();
+	}
+
+}
