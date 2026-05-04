7bae2eeb94f29ef9b106360aa4ae7737811f73c2
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-05-04 14:44:20 -0300

    Change access modifiers to private for properties

--- Lista 9/Exercicio 1/Figura.java
+++ Lista 9/Exercicio 1/Figura.java
@@ -1,9 +1,23 @@
 public class Figura {
-    protected String cor;
-    protected boolean preenchido;
+    private String cor;
+    private boolean preenchido;
 
     public Figura(String cor, boolean preenchido) {
         this.cor = cor;
+        this.preenchido = preenchido;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public boolean isPreenchido() {
+        return preenchido;
+    }
+    public void setPreenchido(boolean preenchido) {
         this.preenchido = preenchido;
     }
 }
