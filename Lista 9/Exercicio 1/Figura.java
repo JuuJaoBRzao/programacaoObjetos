c393f89263a6ea0bb43273a7428990f406b9cfd1
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-05-04 14:45:30 -0300

    Update Figura.java

--- Lista 9/Exercicio 1/Figura.java
+++ Lista 9/Exercicio 1/Figura.java
@@ -1,23 +1,9 @@
 public class Figura {
-    private String cor;
-    private boolean preenchido;
+    protected String cor;
+    protected boolean preenchido;
 
     public Figura(String cor, boolean preenchido) {
         this.cor = cor;
-        this.preenchido = preenchido;
-    }
-
-    public String getCor() {
-        return cor;
-    }
-    public void setCor(String cor) {
-        this.cor = cor;
-    }
-
-    public boolean isPreenchido() {
-        return preenchido;
-    }
-    public void setPreenchido(boolean preenchido) {
         this.preenchido = preenchido;
     }
 }
