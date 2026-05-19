44e09ace89d96ec5f704cb1717353fad15efcf55
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-05-19 10:37:06 -0300

    Create Main.java

---
+++ Lista 12/Exercicio 10/Main.java
@@ -0,0 +1,51 @@
+package pkg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Aluno> listaAluno = new ArrayList<Aluno>();
+		
+		Aluno a1 = new Aluno("Guilherme", 9.9, 9.5);
+		Aluno a2 = new Aluno("Bruno", 9.8, 9);
+		Aluno a3 = new Aluno("Leonardo", 10, 10);
+		
+		listaAluno.add(a1);
+		listaAluno.add(a2);
+		listaAluno.add(a3);
+		
+		exibirAlunos(listaAluno);
+	}
+
+	public static void exibirAlunos(List<Aluno> lista) {
+		for (Aluno a : lista) {
+			System.out.println("Nome: "+a.getNome());
+			System.out.println("Nota 1: "+a.getNota1());
+			System.out.println("Nota 2: "+a.getNota2());
+			System.out.println("Media: "+a.calcularMedia());
+			System.out.println("\n");
+		}
+	}
+	
+	public static void exibirAluno2(List<Aluno> lista) {
+		for (int i= 0; i < lista.size(); i++) {
+			System.out.println("Nome: "+lista.get(i).getNome());
+			System.out.println("Nota 1: "+lista.get(i).getNota1());
+			System.out.println("Nota 2: "+lista.get(i).getNota2());
+			System.out.println("Media: "+lista.get(i).calcularMedia());
+			System.out.println("\n");
+		}
+	}
+	
+	public static void exibirAluno3(List<Aluno> lista) {
+		for (int i= 0; i < lista.size(); i++) {
+			System.out.println("Nome: "+lista.get(i).getNome());
+			System.out.println("Nota 1: "+lista.get(i).getNota1());
+			System.out.println("Nota 2: "+lista.get(i).getNota2());
+			System.out.println("Media: "+lista.get(i).calcularMedia());
+			System.out.println("\n");
+		}
+	}
+}
