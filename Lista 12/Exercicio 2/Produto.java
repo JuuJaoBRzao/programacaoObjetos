fcdbcb2845a2a25b4fe3733cfd3ce1a8d75bb7c1
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-05-19 10:29:57 -0300

    Create Produto class with name and price attributes

---
+++ Lista 12/Exercicio 2/Produto.java
@@ -0,0 +1,13 @@
+public class Produto {
+    protected String nome;
+    protected double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public void desconto() {
+        System.out.println("Desconto padrão aplicado.");
+    }
+}
