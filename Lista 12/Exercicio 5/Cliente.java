93f7ed84bfb2ae96990ff6bd7bb5b1715b960aca
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-05-19 09:15:45 -0300

    Add Cliente class extending Produto

---
+++ Lista 12/Exercicio 5/Cliente.java
@@ -0,0 +1,12 @@
+package pkg;
+
+public class Cliente extends Produto {
+	String nome;
+    double desconto;
+
+    public Cliente(String nome, double desconto) {
+        this.nome = nome;
+        this.desconto = desconto;
+    }
+	
+}
