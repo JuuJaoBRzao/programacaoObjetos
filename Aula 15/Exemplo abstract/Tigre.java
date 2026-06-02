26c6f1a84166a379b0f953be82fb6303befa4d5f
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-06-02 10:08:07 -0300

    Create Tigre.java

---
+++ Aula 15/Exemplo abstract/Tigre.java
@@ -0,0 +1,15 @@
+package projeto;
+
+public class Tigre extends Animal{
+
+	@Override
+	public void emiteSom() {
+		System.out.println("seu merda");
+		
+	}
+	
+	public void cuidarPatio() {
+		System.out.println("CUIDADO!!!! tigre bravo, não come a 1 mês, igual vc a sua mulher, eita porra, bagulho ta serio");
+	}
+	
+}
