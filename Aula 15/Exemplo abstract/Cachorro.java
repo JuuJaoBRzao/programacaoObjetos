ffa2f140f4a311867895c1bfd35b28bb717cdcdf
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-06-02 10:07:50 -0300

    Create Cachorro.java

---
+++ Aula 15/Exemplo abstract/Cachorro.java
@@ -0,0 +1,15 @@
+package projeto;
+
+public class Cachorro extends Animal{
+
+	public void cuidarPatio() {
+		System.out.println("balalblask");
+	}
+	
+	@Override
+	public void emiteSom() {
+		System.out.println("vai toma no cu");
+		
+	}
+
+}
