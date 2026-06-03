e429fea4dd06be76c53bb498e140aba5b5cc382e
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-06-03 19:00:56 -0300

    Update Conta.java

--- Lista 13/Exercicio 1, 2 e 3/Conta.java
+++ Lista 13/Exercicio 1, 2 e 3/Conta.java
@@ -1,8 +1,20 @@
 public abstract class Conta {
 
-    public double saldo;
-    public double limite;
+    protected double saldo;
+    protected double limite;
+
+    public Conta(double saldo, double limite) {
+        this.saldo = saldo;
+        this.limite = limite;
+    }
 
     public abstract void depositar(double valor);
     public abstract void sacar(double valor);
+
+    public double getSaldo() { return saldo; }
+    public double getLimite() { return limite; }
+
+    public void exibirSaldo() {
+        System.out.printf("  Saldo: R$ %.2f | Limite: R$ %.2f%n", saldo, limite);
+    }
 }
