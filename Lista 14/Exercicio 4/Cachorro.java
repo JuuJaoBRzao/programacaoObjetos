2aecedd2bcf5b3de83312a70c51ed3af054f5c8e
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-06-15 15:35:29 -0300

    Create Cachorro.java

---
+++ Lista 14/Exercicio 4/Cachorro.java
@@ -0,0 +1,8 @@
+class Cachorro extends Animal {
+    public Cachorro(String nome, int idade) { super(nome, idade); }
+
+    @Override
+    public void emitirSom() {
+        System.out.println(getNome() + " diz: Au au!");
+    }
+}
