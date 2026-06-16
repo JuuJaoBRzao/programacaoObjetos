ae9caf265789c183073344d60433d580529e8de6
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-06-16 08:44:29 -0300

    Create Main.java

---
+++ aula 17/Exemplos arquivo 2/Main.java
@@ -0,0 +1,37 @@
+package pkg;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+class Main {
+
+	public static void main(String[] args) {
+		try { 
+			FileReader arquivo = new FileReader("alunos.txt");
+			BufferedReader leitor = new BufferedReader(arquivo);
+			
+			System.out.println("Alunos lidos no arquivo:");
+			
+			String linha;
+			while ((linha = leitor.readLine()) != null) {
+				String[] campos = linha.split(",");
+				
+				String nome = campos[0];
+				int idade = Integer.parseInt(campos[1]);
+				
+				Aluno aluno = new Aluno(nome, idade);
+				
+				System.out.println("Nome: " + aluno.getNome() +  ", Idade: " + aluno.getIdade());
+			}
+			leitor.close();
+			arquivo.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
