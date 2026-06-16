eb6bd8374227f124b32bc54da51c1b410dbb4f24
Guilherme Carlin Gonsalves <guilherme.cgonsalves@ufn.edu.br>
2026-06-16 10:53:13 -0300

    Create Pessoa class for serialization example

---
+++ aula 17/Exemplo serialização 2/Pessoa.java
@@ -0,0 +1,21 @@
+package projeto;
+
+public class Pessoa {
+	private String nome;
+	private int idade;
+	
+	public Pessoa(String nome, int idade) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+}
